1a051e1ba74d27596cafcae31cf3629c09764f8c
Святослав <swja7ik@mail.ru>
2024-06-01 01:06:08 +0300

    backend_url fix default config value

--- frontend/src/ml_ops_service/config.py
+++ frontend/src/ml_ops_service/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Config(BaseSettings):
-    backend_url: str = "http://localhost:8000/"
+    backend_url: str = "http://localhost:8000"
     model_config = SettingsConfigDict(env_file=".env")
 
 
